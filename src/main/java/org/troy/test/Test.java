7c9e9df48582e4b93d9428c878335c6157f2bfb2
kouhongyu <kouhongyu@163.com>
2013-07-14 15:08:55 +0800

    测试

---
+++ src/main/java/org/troy/test/Test.java
@@ -0,0 +1,11 @@
+package org.troy.test;
+
+/**
+ * Created by IntelliJ IDEA.<p/>
+ * User: troy-kou<p/>
+ * Date: 13-7-14<p/>
+ * Time: 下午3:05<p/>
+ * Email:kouhongyu@163.com<p/>
+ */
+public class Test {
+}
